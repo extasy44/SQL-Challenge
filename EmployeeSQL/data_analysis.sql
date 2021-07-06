54e6e8f795f2ea9a2a2a1024386f33f8238a9246
extasy44 <seoheejun@gmail.com>
2021-07-06 19:07:03 +1000

    dep, emp in sales

--- EmployeeSQL/data_analysis.sql
+++ EmployeeSQL/data_analysis.sql
@@ -9,14 +9,39 @@ SELECT first_name, last_name, hire_date
 FROM employees
 WHERE hire_date LIKE '%1986'
 
--- List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+-- List the manager of each department with the following information: 
+-- department number, department name, the manager's employee number, last name, first name.
+SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name 
+FROM departments
+JOIN dept_manager
+ON departments.dept_no = dept_manager.dept_no
+JOIN employees
+ON dept_manager.emp_no = employees.emp_no;
 
+-- List the department of each employee with the following information: 
+-- employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no;
 
--- List the department of each employee with the following information: employee number, last name, first name, and department name.
 
 -- List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex
+FROM employees
+WHERE first_name = 'Hercules'
+AND last_name LIKE 'B%';
 
 -- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+JOIN employees
+ON dept_emp.emp_no = employees.emp_no
+JOIN departments
+ON dept_emp.dept_no = departments.dept_no
+WHERE departments.dept_name = 'Sales';
 
 -- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
 
